95eae8a76357b0fe9d8b05286583b1152e6eda98
Doruk Demirci <doruk@marketyo.com>
2021-06-01 10:50:38 +0300

    FE- Expense component refactored

--- Frontend/expense-tracker-frontend/src/App.js
+++ Frontend/expense-tracker-frontend/src/App.js
@@ -1,10 +1,38 @@
+import {useState, useEffect} from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 import './App.css';
 
 function App() {
 
+  let now = new Date();
+  const [expenseList, setExpenseList] = useState([]);
+  const [filteredMonth, setFilteredMonth] = useState(now.getMonth());
+  const [filteredYear, setFilteredYear] = useState(now.getFullYear());
+
+  const fetchExpenses = () => {
+    console.log('FETCH');
+    let statusCode;
+    let url = `http://localhost:3000/expense?month=${filteredMonth}&year=${filteredYear}`;
+    
+    fetch(url, {
+      method: 'GET'
+    })
+    .then(res => {
+        statusCode = res.status;
+        return res.json();
+    })
+    .then(resData => {
+      if (statusCode === 200)
+        setExpenseList(resData.expenses);
+      else
+        console.log('Error occured');
+    })
+    .catch(err => console.log(err));
+  }
+
   const addExpenseHandler = expense => {
+    console.log('ADD');
     let url = 'http://localhost:3000/expense';
     fetch(url,{
       method: 'POST',
@@ -18,13 +46,27 @@ function App() {
         console.log('Adding an expense is failed');
       }
       return res.json();
-    }).catch(err => console.log(err));
+    })
+    .then(() => fetchExpenses())
+    .catch(err => console.log(err));
   }
+
+  useEffect(() => {
+    console.log('WILL BE FETCHED');
+    fetchExpenses();
+  },[filteredMonth, filteredYear])
+
 
   return (
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
-      <Expenses /> 
+      <Expenses 
+        expenseList ={expenseList} 
+        filteredMonth = {filteredMonth} 
+        filteredYear = {filteredYear} 
+        monthChangedHandler = {setFilteredMonth}
+        yearChangedHandler = {setFilteredYear}
+      /> 
     </div>
   );
 }
--- Frontend/expense-tracker-frontend/src/components/Expenses/Expenses.js
+++ Frontend/expense-tracker-frontend/src/components/Expenses/Expenses.js
@@ -1,42 +1,27 @@
-import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
 import ExpenseFilter from './ExpensesFilter';
 import classes from './Expenses.module.css';
 
-function Expenses() {
-  let now = new Date();
-  const [expenseList, setExpenseList] = useState([]);
-  const [filteredMonth, setFilteredMonth] = useState(now.getMonth());
-  const [filteredYear, setFilteredYear] = useState(now.getFullYear());
+const Expenses = props => {
 
-  useEffect(() => {
-    let statusCode;
-    fetch(`http://localhost:3000/expense?month=${filteredMonth}&year=${filteredYear}`, {
-      method: 'GET'
-    })
-      .then(res => {
-        statusCode = res.status;
-        return res.json();
-      })
-      .then(resData => {
-        if (statusCode === 200)
-          setExpenseList(resData.expenses);
-        else
-          console.log('Error occured');
-      })
-      .catch(err => console.log(err));
-  }, [filteredMonth, filteredYear]);
+  const handleFilteredMonth = (value) => {
+    props.monthChangedHandler(value)
+  };
+
+  const handleFilteredYear = (value) => {
+    props.yearChangedHandler(value)
+  };
 
   return (
     <div>
       <Card className={classes.expenses}>
         <ExpenseFilter
-          selectedMonth={filteredMonth}
-          selectedYear={filteredYear}
-          monthHandler={setFilteredMonth}
-          yearHandler={setFilteredYear} />
-        <ExpensesList expenses={expenseList} />
+          selectedMonth={props.filteredMonth}
+          selectedYear={props.filteredYear}
+          monthHandler={handleFilteredMonth}
+          yearHandler={handleFilteredYear} />
+        <ExpensesList expenses={props.expenseList} />
       </Card>
     </div>
   )
