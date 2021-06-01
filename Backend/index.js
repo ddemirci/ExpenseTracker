a8eb1000fca8e9b2723590934a3bafc1a0dd065d
Doruk Demirci <doruk@marketyo.com>
2021-06-01 11:10:30 +0300

    BE- Allowed origin is placed in .env file

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}/${process.env.MONGO_DEFAULT_DATABASE}`
 
 // ======== CORS Policy =======
-const whitelist = ['http://127.0.0.1:3001', 'http://localhost:3001'];
+const allowedPort = process.env.ALLOWED_ORIGIN_PORT;
+const whitelist = [`http://127.0.0.1:${allowedPort}`, `http://localhost:${allowedPort}`];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 // Allow if origin found in whitelist
